6900689fc3ca20b0002f10b32830db5815994299
Steven X. Han <steven@stevenhan.net>
2022-02-13 03:02:51 +0000

    Home pagify things...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from "react";
+import { Motion, spring } from "react-motion";
+import Typewriter from 'typewriter-effect';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const TypingEffect = () => {
+
+        const [showIcons, setShowIcons] = useState(false)
+
+        return (
+            <>
+                <Typewriter
+                    options={{
+                        loop: false,
+                        delay: "60"
+                    }}
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString("Hello, ")
+                            .pauseFor(1000)
+                            .typeString("my name is Steven.")
+                            .pauseFor(500)
+                            .typeString("<br />")
+                            .typeString("I am a management consultant")
+                            .pauseFor(200)
+                            .typeString(" with experience")
+                            .typeString("<br />")
+                            .typeString("across federal and state governments.")
+                            .pauseFor(2000)
+                            .typeString("<br />")
+                            .pauseFor(50)
+                            .typeString("<br />")
+                            .pauseFor(50)
+                            .typeString("<br />")
+                            .typeString("Find me here:")
+                            .pauseFor(200)
+                            .callFunction(() => setShowIcons(true))
+                            .pauseFor(100)
+                            .callFunction(() => document.getElementsByClassName("Typewriter__cursor")[0].setAttribute("style", "color: transparent"))
+                            .start()
+                    }}
+                />
+                <Motion style={{ currentOpacity: spring(showIcons ? 1 : 0, { stiffness: 80, damping: 50 }) }}>
+                    {({ currentOpacity }) =>
+                        <div style={{ opacity: currentOpacity }}>
+                            <div className="contact-icons-ctr">
+                                <a href="https://www.linkedin.com/in/stevenxhan/" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faLinkedinIn} size="1x" />
+                                </a>
+                                <a href="mailto:steven@stevenhan.net" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faEnvelope} size="1x" /></a>
+                            </div>
+                        </div>
+                    }
+                </Motion>
+            </>
+        )
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <TypingEffect />
+            </header>
+        </div>
+    );
 }
 
 export default App;
